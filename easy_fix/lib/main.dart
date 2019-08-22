1ad5778dfe5cd79bea4c8b3aee53ef0356a0f645
Hirantha Rathnayake <sahanhirantha@gmail.com>
2019-08-22 12:30:58 +0530

    bug fixed

--- easy_fix/lib/main.dart
+++ easy_fix/lib/main.dart
@@ -1,44 +1,40 @@
+import 'package:easy_fix/signup_page.dart';
 import 'package:flutter/material.dart';
-import 'package:famguard/ui/signup.dart';
-void main(){
-   runApp(MyApp());
-   return;
+
+// import 'package:famguard/ui/signup.dart';
+void main() {
+  runApp(MyApp());
+  return;
 }
 
-class MyApp extends StatelessWidget
-{
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     return new MaterialApp(
       home: new LoginPage(),
-      theme: new ThemeData(
-        primarySwatch: Colors.blue
-        ),
+      theme: new ThemeData(primarySwatch: Colors.blue),
     );
-  } 
+  }
 }
 
-class LoginPage extends StatefulWidget{
-  
-@override
-State createState() => new LoginPageState();
-}
-class LoginPageState extends State<LoginPage>{
-  TextStyle style = TextStyle(fontFamily: 'Montserrat',fontSize: 20.0);
+class LoginPage extends StatefulWidget {
   @override
-  Widget build(BuildContext context){
+  State createState() => new LoginPageState();
+}
+
+class LoginPageState extends State<LoginPage> {
+  TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+  @override
+  Widget build(BuildContext context) {
     final idField = TextFormField(
       keyboardType: TextInputType.text,
       obscureText: true,
       style: style,
       decoration: InputDecoration(
-        contentPadding: EdgeInsets.fromLTRB(20.0,15.0,20.0,15.0),
-        hintText: "ID",
-        
-
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))
-      ),
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "ID",
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
     );
 
     final passwordField = TextFormField(
@@ -46,77 +42,84 @@ class LoginPageState extends State<LoginPage>{
       obscureText: true,
       style: style,
       decoration: InputDecoration(
-        contentPadding: EdgeInsets.fromLTRB(20.0,15.0,20.0,15.0),
-        hintText: "Password",
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))
-      ),
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Password",
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
     );
 
-    final loginButon =Material(
+    final loginButon = Material(
       elevation: 5.0,
       borderRadius: BorderRadius.circular(30.0),
       color: Color(0xff01A0C7),
       child: MaterialButton(
         minWidth: MediaQuery.of(context).size.width,
         padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        onPressed: (){
-          Navigator.of(context).pushNamed('/signup');
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => SignupPage()));
         },
-        child: Text("Login",
-        textAlign: TextAlign.center,
-        style: style.copyWith(color: Colors.white,fontWeight: FontWeight.bold),
+        child: Text(
+          "Login",
+          textAlign: TextAlign.center,
+          style:
+              style.copyWith(color: Colors.white, fontWeight: FontWeight.bold),
         ),
       ),
     );
 
-      final signupButon =Material(
+    final signupButon = Material(
       elevation: 5.0,
       borderRadius: BorderRadius.circular(30.0),
       color: Color(0xff01A0C7),
       child: MaterialButton(
         minWidth: MediaQuery.of(context).size.width,
         padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        onPressed: (){},
-        child: Text("Signup",
-        textAlign: TextAlign.center,
-        style: style.copyWith(color: Colors.white,fontWeight: FontWeight.bold),
+        onPressed: () {},
+        child: Text(
+          "Signup",
+          textAlign: TextAlign.center,
+          style:
+              style.copyWith(color: Colors.white, fontWeight: FontWeight.bold),
         ),
       ),
     );
-
 
     return Scaffold(
-        body: Center(
-          child: Container(
-            color: Colors.orange,
-            child: Padding(
-              padding: const EdgeInsets.all(36.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  SizedBox(
-                    height: 155.0,
-                     child: Image.asset(
-                        "assets/logo.jpg",
-                        fit: BoxFit.contain,
-                      ),
+      body: Center(
+        child: Container(
+          color: Colors.orange,
+          child: Padding(
+            padding: const EdgeInsets.all(36.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  height: 155.0,
+                  child: Image.asset(
+                    "assets/logo.jpg",
+                    fit: BoxFit.contain,
                   ),
-                  SizedBox(height: 45.0),
-                  idField,
-                  SizedBox(height: 25.0),
-                  passwordField,
-                  SizedBox(height: 35.0),
-                  loginButon,
-                  SizedBox(height: 15.0,),
-                  signupButon,
-                  SizedBox(height: 05.0,)
-
-                ],
-              ),
-              ),
+                ),
+                SizedBox(height: 45.0),
+                idField,
+                SizedBox(height: 25.0),
+                passwordField,
+                SizedBox(height: 35.0),
+                loginButon,
+                SizedBox(
+                  height: 15.0,
+                ),
+                signupButon,
+                SizedBox(
+                  height: 05.0,
+                )
+              ],
             ),
           ),
-        );
+        ),
+      ),
+    );
   }
 }
